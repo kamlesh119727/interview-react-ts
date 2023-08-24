c081216ff0634e29e198020609aa9be432050574
Luis Taniça <matallui@gmail.com>
2023-08-24 12:49:04 -0600

    port assigment from Mersive-Technologies/interview-react

---
+++ src/tree/index.tsx
@@ -0,0 +1,19 @@
+import "./index.css";
+
+const Tree = () => {
+  return (
+    <div className="tree">
+      root
+      <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;ant <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;bear <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cat <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dog <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant{" "}
+      <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;frog <br />
+    </div>
+  );
+};
+
+export default Tree;
